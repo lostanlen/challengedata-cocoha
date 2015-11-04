4b55332cfb2d0d3edd67b46bcfeac46ce33cb052
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-04 14:53:39 +0100

    move cell2csv and generate_submission to utilities folder

--- utilities/generate_submission.m
+++ utilities/generate_submission.m
@@ -8,13 +8,15 @@ end
 
 %% Make cell
 firstcol_ids = arrayfun(@num2str, (1001:1200).', 'UniformOutput', false);
-firstcol = cellfun(@(x) ['Id', x], firstcol_ids, 'UniformOutput', false);
+firstcol = cellfun(@(x) ['ID', x], firstcol_ids, 'UniformOutput', false);
 secondcol = arrayfun(@num2str, labels, 'UniformOutput', false);
 csv_cell = [firstcol, secondcol];
+csv_cell = cat(1, {'ID', 'TARGET'}, csv_cell);
 
 %% Write to path
 date_str = datestr(now(),'yy-mm-dd');
-file_path = ['submissions/', date_str, name];
-cell2csv(file_path, csv_cell);
+file_path = ['submissions/', date_str, name, '.csv'];
+delimiter = ',';
+cell2csv(file_path, csv_cell, delimiter);
 end
 
